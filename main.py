fdcc3c14da970efd1f348835caefa7639a02af33
Sagnik Acharyya <sagnik.acharyya@gmail.com>
2026-06-15 18:52:04 +0530

    custom tavily search tool creation

--- main.py
+++ main.py
@@ -1,35 +1,43 @@
 from dotenv import load_dotenv
 import os
-from langchain_core.prompts import PromptTemplate
+
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import SystemMessage, HumanMessage
 
 from langchain_openai import ChatOpenAI
-from langchain_ollama import ChatOllama
+from tavily import TavilyClient
 
 load_dotenv()
 
+tavily = TavilyClient()
+@tool
+def search(query: str) -> str:
+   """Searches on the internet for the given query and returns the result.
+    Args:
+        query (str): The search query.
+    Returns:
+        str: The search result.
+   """
+   print(f"Searching for: {query}")
+   return tavily.search(query=query)
 
 def main():
-    print("Hello from langchain-course!")
-    information = """Narendra Damodardas Modi[a] (born 17 September 1950) is an Indian politician who has served as
-      the prime minister of India since 2014.
-      Modi was the chief minister of Gujarat from 2001 to 2014 and is the member of parliament (MP) for Varanasi. 
-      He is a member of the Bharatiya Janata Party (BJP) and of the Rashtriya Swayamsevak Sangh (RSS), 
-      a right-wing Hindutva paramilitary volunteer organisation. 
-      He is the longest-serving prime minister outside the Indian National Congress."""
-
-    summary_template = """
-    Given the information {info} about the person I want you to create: 
-    1) A summary of his life
-    2) 2 interesting facts about him
-    """
-
-    prompt = PromptTemplate(input_variables=["info"], template=summary_template)
-    # llm = ChatOpenAI(model_name="gpt-5", temperature=0)
-    llm = ChatOllama(model="gemma3:270m", temperature=0)
-    chain = prompt | llm
-    response = chain.invoke(input={"info": information})
-
-    print(response.content)
+    print("Starting the agent...")
+    llm = ChatOpenAI(
+        model="gpt-4o",
+        temperature=0,
+        openai_api_key=os.environ.get("OPENAI_API_KEY"),
+    )
+    tools = [search]
+    agent = create_agent(
+        model=llm,
+        tools=tools
+    )
+    
+    response = agent.invoke({"messages": [HumanMessage(content="List 3 AI engineer job openings in Bangalore and list their details")]})
+    print(response)
+    
 
 
 if __name__ == "__main__":
